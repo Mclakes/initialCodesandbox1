2b17d2e8567ba47688a0953b65dc0253d323c6ee
Olalekan Bankole <60821157+Mclakes@users.noreply.github.com>
2022-04-25 19:41:42 +0100

    change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./component/App.jsx";
+import pi, { doublePi, triplePi } from "./math.js";
+import { add, multiply, subtract, divide } from "./calculator.js";
 
 import "../public/styles.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    <ul>
+      <li>{add(1, 2)}</li>
+      <li>{multiply(2, 3)}</li>
+      <li>{subtract(7, 2)}</li>
+      <li>{divide(5, 2)}</li>
+    </ul>
+    <ul>
+      <li>{pi}</li>
+      <li>{doublePi()}</li>
+      <li>{triplePi()}</li>
+    </ul>
+    <App />
+  </div>,
+  document.getElementById("root")
+);
